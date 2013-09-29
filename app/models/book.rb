b415654ec58811dcb2fcc44c3dcff2136550c595
Alexander K <xpyro@ya.ru>
2013-09-29 22:26:31 +0600

    prev, next gatha links

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
   has_many :vaggas
   has_many :translations, as: :translateable
+  has_many :gathas, through: :vaggas
 
   extend FriendlyId
   friendly_id :index, use: :finders
--- app/models/gatha.rb
+++ app/models/gatha.rb
@@ -1,4 +1,19 @@
 class Gatha < ActiveRecord::Base
   belongs_to :vagga
   has_many :translations, as: :translateable
+
+  # hardcode is not always a problem...
+  MIN = 1
+  MAX = 423
+
+  def prev
+    that = index.to_i - 1
+    that = MAX if that < MIN
+    vagga.book.gathas.where(index: that).first
+  end
+  def next
+    that = index.to_i + 1
+    that = MIN if that > MAX
+    vagga.book.gathas.where(index: that).first
+  end
 end
