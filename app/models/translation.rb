fd53b82d824838b07fa71c59b18a026d04132e0c
Alexander K <xpyro@ya.ru>
2013-10-01 14:18:57 +0600

    marks

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -2,4 +2,11 @@ class Translation < ActiveRecord::Base
   belongs_to :translateable, polymorphic: true
   belongs_to :source
   belongs_to :language
+
+  def excerpt text, from, to
+    phrase = Phrase.where(text: text).first_or_create
+    excerpt = phrase.excerpts
+      .where(translation: self, from: from, to: to)
+      .first_or_create
+  end
 end
